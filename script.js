bf5adbdfed95a5364946d3fad9ba2ca996f9282b
arthurfortunato <luizarthur.fortunato@gmail.com>
2021-12-06 17:45:05 -0300

    Finalizando a responsividade

--- script.js
+++ script.js
@@ -11,16 +11,17 @@ $(document).ready(function(){
       loop: true,
       autoplayTimeOut: 2000,
       autoplayHoverPause: true,
+      responsiveClass:true,
       responsive: {
-          0:{
+          300:{
               items: 1,
               nav: false
           },
-          600:{
+          900:{
               items: 2,
               nav: false
           },
-          1000:{
+          1200:{
               items: 3,
               nav: false
           }
