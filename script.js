df53805d8d9f66e32b0fd42b796200a876a1cbaf
arthurfortunato <luizarthur.fortunato@gmail.com>
2021-12-07 09:35:14 -0300

    Ajustando o autoplay do carousel

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var typed = new Typed(".typing", {
         strings: ["Desenvolvedor", "FullStack Jr"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
-    
-  $('.carousel').owlCarousel({
-      margin: 20,
-      loop: true,
-      autoplayTimeOut: 2000,
-      autoplayHoverPause: true,
-      responsiveClass:true,
-      responsive: {
-          300:{
-              items: 1,
-              nav: false
-          },
-          900:{
-              items: 2,
-              nav: false
-          },
-          1200:{
-              items: 3,
-              nav: false
-          }
-      }
-  });
+
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+
+        responsiveClass: true,
+        responsive: {
+            300: {
+                items: 1,
+                nav: false
+            },
+            900: {
+                items: 2,
+                nav: false
+            },
+            1200: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
 });
